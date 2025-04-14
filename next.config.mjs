a2c38079d488e79eafbc8b8d428fb20b34a49574
gonzalogomezdev <gonzalogomezprojects@gmail.com>
2025-04-14 18:41:05 -0300

    delete files

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true, // Evita que ESLint bloquee el despliegue en Vercel
-      },
-};
-
-export default nextConfig;
